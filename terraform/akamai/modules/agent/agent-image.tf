46dfce8a04c41a1815349476a1855ab3d5241c10
Slava <20563034+veaceslavdoina@users.noreply.github.com>
2026-06-14 21:58:14 +0300

    chore(akamai): add ubuntu-26.04 to the os_name option

--- terraform/akamai/modules/agent/agent-image.tf
+++ terraform/akamai/modules/agent/agent-image.tf
@@ -1,7 +1,9 @@
 # Image
 locals {
   os_name_map = {
-    ubuntu = "linode/ubuntu24.04"
+    "ubuntu-24.04" = "linode/ubuntu24.04"
+    "ubuntu-26.04" = "linode/ubuntu26.04"
+    ubuntu         = "linode/ubuntu24.04"
   }
 
   os_name = lookup(local.os_name_map, var.os_name, var.os_name)
